73afaf6b5c96a433c6c5a32b60fc2a24264fb045
Daison <daisonthms@gmail.com>
2023-07-09 18:19:35 +0530

    window rules for hyperland

--- .config/hypr/startup.sh
+++ .config/hypr/startup.sh
@@ -9,3 +9,6 @@ systemctl --user start wireplumber
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
+
+wallpaper="~/wallpaper/downloads/sky_1.jpg"
+swaybg -o eDP-1 -i "$wallpaper" -o HDMI-A-1 -i "$wallpaper" &
